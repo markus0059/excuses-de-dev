dd515c5889f6d107e846499d773f64a19272b11b
Germe Marc Antoine <markus0059@hotmail.fr>
2023-04-04 17:59:02 +0200

    add generate random message on homepage

--- src/Controller/ExcuseController.php
+++ src/Controller/ExcuseController.php
@@ -6,38 +6,80 @@ use App\Repository\ExcuseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Length;
 
 #[Route('/', name: 'excuse_')]
 class ExcuseController extends AbstractController
 {
-    #[Route('/list', name: 'list')]
-    public function index(ExcuseRepository $excuseRepository): Response
-    {
-        $excuses = $excuseRepository->findAll();
-        return $this->render('excuse/index.html.twig', [
-          'excuses' => $excuses,
-        ]);
-    }
+  #[Route('/', name: 'afficher_donnees')]
+  public function randomExcuse(ExcuseRepository $excuseRepository): Response
+  {
+    // je recupère tous les excuses
+    $excuses = $excuseRepository->findAll();
+    // je prend le nombre total des entités excuses
+    $numbermax= count($excuses);
+    // je créer un index random qui prend en compte le nombre d'élément de mon array excuse
+    // je retire 1 pour l'index 0
+    $indexrandom= rand(0, $numbermax-1);
+    // enfin je recupère l'excuse qui conrespond à l'index de mon tableau d'excuses et cela alétoirement
+    $randexcuse = $excuses[$indexrandom];
 
-    #[Route('/lost', name: 'lost')]
-    public function lost(): Response
-    {
+    return $this->render('base.html.twig', [
+      'randexcuse' => $randexcuse,
+    ]);
 
-      $sentence = "I'm lost";
+  }
 
-      return $this->render('excuse/lost.html.twig', [
-        'sentence' => $sentence,
-      ]);
+  #[Route('/list', name: 'list')]
+  public function index(ExcuseRepository $excuseRepository): Response
+  {
+    $excuses = $excuseRepository->findAll();
 
-    }
+    return $this->render('excuse/index.html.twig', [
+      'excuses' => $excuses,
+    ]);
+  }
 
-    #[Route('/*', name: 'error')]
-    public function error(): Response
-    {
-      $sentence = "Error 404 ";
+  #[Route('/lost', name: 'lost')]
+  public function lost(): Response
+  {
+
+    $sentence = "I'm lost";
+
+    return $this->render('excuse/lost.html.twig', [
+      'sentence' => $sentence,
+    ]);
+  }
+
+  #[Route('/*', name: 'error')]
+  public function error(): Response
+  {
+    $sentence = "";
+
+    return $this->render('excuse/error404.html.twig', [
+      'sentence' => $sentence,
+    ]);
+  }
+
+  #[Route('/$http_code/{excuseHttpCode}', methods: ['GET'], name: 'show')]
+  public function show(string $excuseHttpCode, ExcuseRepository $excuseRepository): Response
+  {
+    $excuse = $excuseRepository->findOneByHttpcode($excuseHttpCode);
+
+
+
+    if (!$excuse) {
+
+      $sentence = 'Pas d \' excuses avec le code http : ' . $excuseHttpCode . ' trouvé';
+
 
       return $this->render('excuse/error404.html.twig', [
         'sentence' => $sentence,
       ]);
     }
+
+    return $this->render('excuse/show.html.twig', [
+      'excuse' => $excuse,
+    ]);
+  }
 }
--- src/Repository/ExcuseRepository.php
+++ src/Repository/ExcuseRepository.php
@@ -63,4 +63,5 @@ class ExcuseRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
+
 }
